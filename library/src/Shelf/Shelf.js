05b7dcb1f7d0dfaeb3f9d3c1f74e5b75a57cddd8
Zihao Zhu <zihao@azzh.me>
2022-11-30 00:50:46 +0800

    add default

--- library/src/Shelf/Shelf.js
+++ library/src/Shelf/Shelf.js
@@ -1,21 +1,60 @@
 import React, { Component } from "react";
 import Book from "../Book/Book";
 
-class Library extends Component {
-  book;
+class Shelf extends Component {
+  constructor(props) {
+    super(props);
 
-  handleAddBookToLibrary(title, author, description, pages) {
-    this.book = new Book(title, author, description, pages);
+    this.state = {
+      books: [
+        {
+          title: "The Two Towers",
+          author: "J.R.R. Tokkien",
+          description: "Part 2 of The Lord of the Rings trilogy.",
+          pages: "500",
+        },
+        {
+          title: "The Gunslinger",
+          author: "Stephen King",
+          description: "A gunslinger roams the wild.",
+          pages: "300",
+        },
+      ],
+    };
+  }
+
+  handleAddBookToLibrary(
+    title,
+    author = "Unknown",
+    description = "No description available",
+    pages = "Unknown"
+  ) {
+    this.setState({
+      books: this.state.books.concat([
+        {
+          title,
+          author,
+          description,
+          pages,
+        },
+      ]),
+    });
   }
 
   render() {
-    return (
-      <div className="library">
-        {this.props.children}
-        {this.book}
-      </div>
-    );
+    const library = this.state.books.map((book) => {
+      return (
+        <Book
+          title={book.title}
+          author={book.author}
+          description={book.description}
+          pages={book.pages}
+        />
+      );
+    });
+
+    return <div className="shelf">{this.renderBooks(library)}</div>;
   }
 }
 
-export default Library;
+export default Shelf;
